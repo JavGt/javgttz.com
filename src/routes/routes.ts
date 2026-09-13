f8c1cfd165644a7c087332f378778dac8256f2de
JavGt <javier.bz.gt@gmail.com>
2026-09-13 09:28:43 -0600

    fix: corregir la ruta de la página de inicio en el enrutador

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,6 @@ import "@/pages/home";
 import "@/pages/cv";
 
 export const ROUTES = [
-  { path: "/javgttz.com", render: () => html`<home-page></home-page>` },
+  { path: "/", render: () => html`<home-page></home-page>` },
   { path: "/cv", render: () => html`<cv-page></cv-page>` }
 ];
